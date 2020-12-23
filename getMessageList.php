15c56479563c7ed48f978c06b7b684d10a1cbbe6
arv78 <arv78@ymail.com>
2020-12-23 19:18:12 +0330

    added this file to get all the messages between two users without refreshing the page(in a SPW manner)

---
+++ getMessageList.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+require_once "congif/config.php";
+require_once "model/user.php";
+
+if(isset($_SESSION['USER'])) {
+    $u = unserialize($_SESSION['USER']);
+    $m = unserialize($_SESSION['MESSAGE']);
+    $tmpMes = new message();
+    $tmpMes->setto($m->getto());
+    $tmpMes->setfrom($u->getUsername());
+    $usersList = $tmpMes->see_messages();
+    echo json_encode($usersList);
+}
