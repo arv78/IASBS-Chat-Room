c58253bbce7259115910208682d132a155d96a6f
arv78 <arv78@ymail.com>
2020-12-25 11:26:17 +0330

    added this file to be able to get the profile when clicked on the profile link in the MessagePage

---
+++ getProfile.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+require_once "congif/config.php";
+require_once "model/user.php";
+
+if(isset($_SESSION['USER'])) {
+    $u = unserialize($_SESSION['USER']);
+    $user_temp = new user();
+    $user_temp->setUsername($u->getUsername());
+    $profile = $user_temp->Getprofile();
+    echo json_encode($profile);
+}
+
+?>
