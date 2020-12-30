3786294095bffc2d9548ede7f8ec39efeecbe5f9
Naivd Khalili <35469386+navidkha@users.noreply.github.com>
2020-12-30 18:53:37 +0330

    delete some unnecessary variables in userlist.js

--- js/userlist.js
+++ js/userlist.js
@@ -1,23 +1,13 @@
 var app = angular.module('UsersListApp', ['ui.bootstrap']);
 
+
+
 app.controller('UsersListController', function ($scope, $http) {
-    $scope.showLoader = true;
-    $http.get('getUsersList.php').then(function (d) {
+    
+    $http.get('getContactList.php').then(function (d) {
+        alert('here');
         $scope.lst = d.data;
-        $scope.totalItems = $scope.lst.length;
-        $scope.currentPage = 1;
-        $scope.numPerPage = 5;
-
-        $scope.paginate = function (value) {
-            var begin, end, index;
-            begin = ($scope.currentPage - 1) * $scope.numPerPage;
-            end = begin + $scope.numPerPage;
-            index = $scope.lst.indexOf(value);
-            return (begin <= index && index < end);
-        };
-
-        $scope.showLoader = false;
-
+        
     }, function (error) {
         alert('failed to load users list');
     });
