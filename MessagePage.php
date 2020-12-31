e20bbeff2172ed70bf024d05d80d5193b5a0c445
arv78 <arv78@ymail.com>
2020-12-31 12:34:56 +0330

    fixed choosing a contact error

--- MessagePage.php
+++ MessagePage.php
@@ -9,15 +9,17 @@ else
     $u = unserialize($_SESSION['USER']);
     $WelcomeMessage = 'Welcome '.$u->getName(). ' '.$u->getFamily();
 
-    unset($_SESSION['MESSAGE']);
-    $m = new message();
-    $m->setto($_POST['contact_username']);
-    $_SESSION['MESSAGE'] = serialize($m);
+    if ($_POST['contact_username_submit']) {
+        unset($_SESSION['MESSAGE']);
+        $m = new message();
+        $m->setto($_POST['contact_username']);
+        $_SESSION['MESSAGE'] = serialize($m);
+    }
 }
 
 require "config/config.php";
-include "view/shared/messeges.html";
 include "view/shared/contacts.html";
+include "view/shared/messeges.html";
 
 
 ?>
